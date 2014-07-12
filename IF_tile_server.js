aa7c5e2e0dd08f0efb8a7488bcdc6a4fa5712c7f
jrbaldwin <jrbaldwin@gmail.com>
2014-07-11 23:08:19 -0400

    asfd

--- IF_tile_server.js
+++ IF_tile_server.js
@@ -61,28 +61,101 @@ var tempVRT = './maps/newtesting9.vrt';
 var tempVRT2 = './maps/newtesting10.vrt';
 
 var exec = require('child_process').exec;
-exec('gdal_translate -of VRT -a_srs EPSG:4326 -gcp '+ nw_pixel_lng +' '+ nw_pixel_lat +' '+ nw_loc_lng +' '+ nw_loc_lat +' -gcp '+ sw_pixel_lng +' '+ sw_pixel_lat +' '+ sw_loc_lng +' '+ sw_loc_lat +' -gcp '+ ne_pixel_lng +' '+ ne_pixel_lat +' '+ ne_loc_lng +' '+ ne_loc_lat +' -gcp '+ se_pixel_lng +' '+ se_pixel_lat +' '+ se_loc_lng +' '+ se_loc_lat +' '+ tempIMG +' '+ tempVRT + '', function(err, stdout, stderr) {
-	// React to callback
-	console.log(stderr);
-	console.log(stdout);
 
-	exec('gdalwarp -of VRT -t_srs EPSG:4326 '+tempVRT+' '+tempVRT2+'', function(err2, stdout2, stderr2) {
+// //build VRT file from image pixels && location coordinates
+// exec('gdal_translate -of VRT -a_srs EPSG:4326 -gcp '+ nw_pixel_lng +' '+ nw_pixel_lat +' '+ nw_loc_lng +' '+ nw_loc_lat +' -gcp '+ sw_pixel_lng +' '+ sw_pixel_lat +' '+ sw_loc_lng +' '+ sw_loc_lat +' -gcp '+ ne_pixel_lng +' '+ ne_pixel_lat +' '+ ne_loc_lng +' '+ ne_loc_lat +' -gcp '+ se_pixel_lng +' '+ se_pixel_lat +' '+ se_loc_lng +' '+ se_loc_lat +' '+ tempIMG +' '+ tempVRT + '', function(err, stdout, stderr) {
+// 	// React to callback
+// 	console.log(stderr);
+// 	console.log(stdout);
 
-		console.log(stderr2);
-		console.log(stdout2);
+// 	//warp VRT to earth curvature
+// 	exec('gdalwarp -of VRT -t_srs EPSG:4326 '+tempVRT+' '+tempVRT2+'', function(err2, stdout2, stderr2) { 
+
+// 		console.log(stderr2);
+// 		console.log(stdout2);
+
+// 		//build tiles from warped VRT 
+// 		exec('gdal2tiles.py -k -n -w none '+tempVRT2+'', function(err3, stdout3, stderr3) {
+
+// 			console.log(stderr3);
+// 			console.log(stdout3);
+
+// 			//delete temp img and vrt
+
+// 		});
+
+// 	});
+// });
+
+var myCallback = function(data) {
+  console.log('got data: '+data);
+};
+
+var usingItNow = function(callback) {
+  callback('get it?');
+};
+
+usingItNow(myCallback);
 
 
-		exec('gdal2tiles.py -k -n -w none '+tempVRT2+'', function(err3, stdout3, stderr3) {
 
-			console.log(stderr3);
-			console.log(stdout3);
+    //disabled Max image upload size for NOW << enable later...
+   // if (req.files.files[0].size <= 5242880){
 
-			//delete temp img and vrt
+        // SET FILE SIZE LIMIT HERE 
+        //FILTER ANYTHING BUT GIF JPG PNG
+function saveImage(callback){
 
-		});
+      var fstream;
+        req.pipe(req.busboy);
 
-	});
-});
+        req.busboy.on('file', function (fieldname, file, filename) {
+
+            var fileName = filename.substr(0, filename.lastIndexOf('.')) || filename;
+            var fileType = filename.split('.').pop();
+
+            while (1) {
+
+                var fileNumber = Math.floor((Math.random()*100000000)+1); //generate random file name
+                var fileNumber_str = fileNumber.toString(); 
+                var current = fileNumber_str + '.' + fileType;
+
+                //checking for existing file, if unique, write to dir
+                if (fs.existsSync("app/uploads/" + current)) {
+                    continue; //if there are max # of files in the dir this will infinite loop...
+                } 
+                else {
+
+                    var newPath = "app/uploads/" + current;
+
+                    fstream = fs.createWriteStream(newPath);
+                    file.pipe(fstream);
+                    fstream.on('close', function () {
+                         im.crop({
+                          srcPath: newPath,
+                          dstPath: newPath,
+                          width: 100,
+                          height: 100,
+                          quality: 85,
+                          gravity: "Center"
+                        }, function(err, stdout, stderr){
+
+                            //res.send("uploads/"+current);
+                            return current; 
+
+                        });                       
+                    });
+
+                    break;
+                }
+            }
+
+        });
+
+}
+  
+
+
 
 
 
